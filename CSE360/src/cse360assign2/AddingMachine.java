741b08955eea309f40d36d48fed93213dccd3d07
Alwin Tomy <atomy1@asu.edu>
2019-10-06 22:38:15 -0700

    Updated methods to be able to do actual calculations

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,33 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String calculation; // Store calculations
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		calculation = "0 "; // starts calculation at 0
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		calculation += " + " + value; // update calculations
+		total += value;
 	}
 	
 	public void subtract (int value) {
-		
+		calculation += " - " + value; // update calculations
+		total -= value;
 	}
 		
 	public String toString () {
-		return "";
+		return calculation;
 	}
 
 	public void clear() {
-	
+		total = 0; 
+		calculation = "0 ";
 	}
 }
